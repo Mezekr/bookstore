43f5a25a7fa61852af5525584da1d09b7bb8746a
Mezekr W <mezi.edu@tuta.io>
2025-01-21 16:09:38 +0100

    create a basic book UI element

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ function App() {
 
   return (
     <>
+
+     <h1>Book Store</h1>
       <div>
-        
+       <input type='text' placeholder='Book Tittle'/>
+       <input type='number' placeholder='Realse year'/>
+       <button type='submit' >Add Book</button>
+
       </div>
     </>
   )
