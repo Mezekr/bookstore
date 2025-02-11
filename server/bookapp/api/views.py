3e9ad59391d57fcb30461d6aac650a17e0d5bab3
Mezekr W <mezi.edu@tuta.io>
2025-02-11 17:55:43 +0100

    add an API endpoint to retrieve all books

--- server/bookapp/api/views.py
+++ server/bookapp/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .models import Book
+from rest_framework.response import Response
+from .serializer import BookSerializer
+
 
 # Create your views here.
+@api_view(['GET'])
+def get_books(request):
+    books = Book.objects.all()
+    serialized_data= BookSerializer(books, many=True).data
+    return Response(serialized_data)
+    
