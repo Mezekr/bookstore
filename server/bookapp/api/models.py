c883e049f756b59537b04658d766c72c5615c6c3
Mezekr W <mezi.edu@tuta.io>
2025-02-11 19:03:02 +0100

    fix typo mistake in book model field name

--- server/bookapp/api/models.py
+++ server/bookapp/api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    relase_year = models.IntegerField()
+    release_year = models.IntegerField()
 
     def __str__(self):
         return self.title
