a983eb5797d75a6f8543423e69c00397ff6cc6d6
Mezekr W <mezi.edu@tuta.io>
2025-02-11 19:13:55 +0100

    add an API endpoint to add or create book data

--- server/bookapp/api/views.py
+++ server/bookapp/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from .models import Book
 from rest_framework.response import Response
 from .serializer import BookSerializer
+from rest_framework import status
 
 
 # Create your views here.
@@ -11,3 +12,11 @@ def get_books(request):
     serialized_data= BookSerializer(books, many=True).data
     return Response(serialized_data)
     
+@api_view(['POST'])
+def create_book(request):
+    
+    serialized_data= BookSerializer(data= request.data)
+    if serialized_data.is_valid():
+        serialized_data.save()
+        return Response(serialized_data.data, status=status.HTTP_201_CREATED)
+    return Response(serialized_data.error, status=status.HTTP_400_BAD_REQUEST)
