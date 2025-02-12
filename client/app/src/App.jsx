9d63fdb9ee13f7f366cc7b05ecd16ac172e8e9a8
Mezekr W <mezi.edu@tuta.io>
2025-02-12 21:13:12 +0100

    add data fetching functionality

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -1,6 +1,28 @@
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
+  const [books, setBooks] = useState([])
+
+  const BASE_URL = "http://127.0.0.1:8000/api/books"
+
+  const fetchBooks = async ()=>{
+    try {
+      const response = await fetch(`${BASE_URL}`)
+      if(!response.ok){
+          console.error("Something went wrong");
+      }
+      const data = await response.json()
+      // console.log(data);
+      setBooks(data)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchBooks()
+  }, [])
   
 
   return (
@@ -12,6 +34,17 @@ function App() {
        <input type='number' placeholder='Realse year'/>
        <button type='submit' >Add Book</button>
 
+      </div>
+      <div>
+        {books.length <= 0 ? (<h1>NO Book to display</h1>):(
+          books.map((book) =>{
+            return <div key={book.id}>
+              <p>Title: { book.title}</p>
+              <p>Release Year: { book.release_year}</p>
+            </div>
+})
+        )}
+        
       </div>
     </>
   )
